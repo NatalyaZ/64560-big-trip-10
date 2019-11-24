29b1771c920559be3561e4b706d534f332a4c110
natoosique <zn@sletat.ru>
2019-11-24 20:28:27 +0300

    исправления по eslint

--- src/components/trip-info.js
+++ src/components/trip-info.js
@@ -1,8 +1,6 @@
 export const renderTripInfoTemplate = () => (
   `<div class="trip-info__main">
     <h1 class="trip-info__title">Amsterdam &mdash; ... &mdash; Amsterdam</h1>
-
     <p class="trip-info__dates">Mar 18&nbsp;&mdash;&nbsp;21</p>
   </div>`
 );
-  
